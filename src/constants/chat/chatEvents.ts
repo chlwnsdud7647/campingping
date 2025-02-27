845b381f8326bdbe903d3feca8987cb5c4db4885
yeon <yeonnnnn.k@gmail.com>
2025-02-27 19:59:54 +0900

    🧑‍💻 Improve: 채팅 관련 이벤트 상수화

---
+++ src/constants/chat/chatEvents.ts
@@ -0,0 +1,17 @@
+export const CHATS = {
+  ROOM: {
+    CREATE: 'createRoom',
+    CREATED: 'roomCreated',
+    GET: 'getChatRooms',
+    RECEIVE: 'chatRooms',
+  },
+  SENDER: {
+    SEND: 'sendMessage',
+    READ: 'updateRead',
+  },
+  HISTORY: {
+    GET: 'getChatHistory',
+    RECEIVE: 'newMessage',
+    READ: 'openChatRoom',
+  },
+};
